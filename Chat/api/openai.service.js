e8734615390e04a1060488dedd4570230222262d
Ryan Schultz <rdschultz@verisk.com>
2023-04-04 19:01:30 -0600

    Added interfaces and enums

--- Chat/api/openai.service.js
+++ Chat/api/openai.service.js
@@ -1,4 +1,19 @@
 import { config } from "dotenv";
 config();
 
-console.log(config.env.API_KEY);
+import { Configuration, OpenAIApi } from "openai";
+
+const openai = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
+);
+
+openai
+  .createChatCompletion({
+    model: "gpt-3.5-turbo",
+    message: [{ role: "user", content: "Hello" }],
+  })
+  .then((res) => {
+    return res;
+  });
---
+++ Chat/src/app/enums/chat.ts
@@ -0,0 +1,4 @@
+enum ChatMessageUserType {
+    USER,
+    BOT
+}

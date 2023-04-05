0d04b48fd18cecadace5bb864b6e0215e0700cce
Ryan Schultz <rdschultz@verisk.com>
2023-04-04 20:52:36 -0600

    Added the chat service, chat component, a few data pieces.

--- Chat/src/app/components/chat/chat.component.ts
+++ Chat/src/app/components/chat/chat.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { ChatService } from '../../services/chatService/chat.service';
+import {IChatMessage} from "../../interfaces/chat";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit{
+  @ViewChild('content') contentInput?: ElementRef;
+
+  messages: IChatMessage[] = [];
+  constructor(private chatService: ChatService) {
+  }
+  ngOnInit(){
+    this.messages = this.chatService.getMessages()
+  }
+
+  sendMessage(content: string, event: Event): void {
+    event.preventDefault()
+    this.chatService.addMessage("User", content)
+    if(this.contentInput){
+      this.contentInput.nativeElement.value = '';
+    }
+  }
 
 }

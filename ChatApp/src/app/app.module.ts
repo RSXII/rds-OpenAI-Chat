9c4f1492a549cd924ed82cad206e317d521101dd
Ryan Schultz <rdschultz@verisk.com>
2023-04-15 11:25:32 -0600

    Added firebase auth service.

--- ChatApp/src/app/app.module.ts
+++ ChatApp/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { SettingsComponent } from './components/settings/settings.component';
 import { MainComponent } from './main/main.component';
 import { ButtonComponent } from './components/button/button.component';
 import {NgOptimizedImage} from "@angular/common";
+import { environment } from "../environments/environments";
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireStorageModule} from "@angular/fire/compat/storage";
+import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
 
 @NgModule({
   declarations: [
@@ -19,7 +25,10 @@ import {NgOptimizedImage} from "@angular/common";
     MainComponent,
     ButtonComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage],
+  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage,
+    AngularFireModule.initializeApp(environment.firebase), AngularFireAuthModule,
+    AngularFirestoreModule, AngularFireStorageModule, AngularFireDatabaseModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
